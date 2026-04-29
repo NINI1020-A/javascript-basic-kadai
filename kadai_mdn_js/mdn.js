af10053593706052484db9c5115bafef821da1ed
NINI1020-A <nishiyusa@gmail.com>
2026-04-29 20:26:14 +0900

    Update mdn.js

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,2 +1,2 @@
 let date = new Date();
-console.log(date.getFullYear() + "年"+date.getMonth()+"月"+date.getDate()+"日");
+console.log(date.getFullYear() + "年"+(date.getMonth()+1)+"月"+date.getDate()+"日");
