07b8200589b5e92b3040e55a9140f6e56a405d7f
NINI1020-A <nishiyusa@gmail.com>
2026-04-29 19:47:58 +0900

    Update bmi.js

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,10 +1,12 @@
 // 変数の宣言
 let bodyWeight;
 let height;
+let bmi;
 
 // 値の代入
 bodyWeight = 68;
 height = 1.7;
+bmi = bodyWeight/(height**2);
 
 // オブジェクトの値を出力する
-console.log(bodyWeight/(height**2));
+console.log(bmi);
